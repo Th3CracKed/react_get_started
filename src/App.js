5b55e070d143ffcfcaa1bc836529eff3fb989284
DRIOWYA abdelghafour <abdodriowya@gmail.com>
2020-07-17 14:23:23 +0200

    connect to mock api, one way data flow

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import SearchParams from "./searchParams";
 // declared component
 const App = () => {
   return (
-    <div>
-      <h1 id="something-important">Adopt Me!</h1>
-      <SearchParams />
-    </div>
+    <React.StrictMode>
+      <div>
+        <h1 id="something-important">Adopt Me!</h1>
+        <SearchParams />
+      </div>
+    </React.StrictMode>
   );
 };
 // inject it into the dom, root div
